ed4cf3dc04bab2e733ccfad5e0cb8ae1c9754946
ianfrantz <ian@ianfrantz.com>
2017-01-15 12:09:08 -0800

    Added all the code for Challenge 1

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 #Hadley's Presentation from November 2016
 The purpose of this project is to code each of the eight examples from Hadley's November 2016 talk at the NYC Datavis meetup: [https://speakerdeck.com/hadley/eight-visualisation-challenges-with-ggplot2](https://speakerdeck.com/hadley/eight-visualisation-challenges-with-ggplot2)
 
-
--- eight-challenges-rscript.R
+++ eight-challenges-rscript.R
@@ -1,3 +1,12 @@
 library("ggplot2")
 
 #Example 1 - Labelling plots with the labs() function
+
+ggplot(mpg, aes(displ, hwy))+
+  geom_point(aes(color = class))+
+  geom_smooth(se = FALSE, method = "loess")+
+  labs(
+    title = "Fuel efficiency generally decreases with engine size",
+    subtitle = "Two seaters (sports cars) are an exception because of their light weight",
+    caption = "Data from fueleconomy.gov"
+  )
