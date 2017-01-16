05dd42cfda249f801bc44ad1a6f5483722654cf5
ianfrantz <ian@ianfrantz.com>
2017-01-15 16:00:16 -0800

    Clean up

--- eight-challenges-rscript.R
+++ eight-challenges-rscript.R
@@ -6,7 +6,7 @@ ggplot(mpg, aes(displ, hwy))+
   geom_point(aes(color = class))+
   geom_smooth(se = FALSE, method = "loess")+
   labs(
-    title = "Fuel efficiency generally decreases with engine size",
-    subtitle = "Two seaters (sports cars) are an exception because of their light weight",
-    caption = "Data from fueleconomy.gov"
+    title = "Title: Fuel efficiency generally decreases with engine size",
+    subtitle = "Subtitle: Two seaters (sports cars) are an exception because of their light weight",
+    caption = "Caption: Data from fueleconomy.gov"
   )
